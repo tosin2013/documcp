cf26e8b3463492fb5cfa1c81c6a9908fc5b42559
Tosin Akinosho <tosin.akinosho@gmail.com>
2025-09-10 20:28:22 -0400

    ci: add commitlint validation and automated release workflow

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New features
+        'fix',      // Bug fixes
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting, etc.)
+        'refactor', // Code refactoring
+        'test',     // Test additions/modifications
+        'chore',    // Maintenance tasks
+        'perf',     // Performance improvements
+        'ci',       // CI/CD changes
+        'build',    // Build system changes
+        'revert'    // Revert commits
+      ]
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never']
+  }
+};
